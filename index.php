ceb60dc45b9b3d177042b200cc596586dcf2dff9
Julien <julien.dousse@gmail.com>
2022-11-23 16:34:27 +0100

    Exercice à faire

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
-require_once 'models/Task.php';
+require_once  'src/controllers/TaskController.php';
+require_once  'src/controllers/UserController.php';
 
-$task = new Task();
+// monsite.fr/index.php?controller=task&action=index
 
-$t = $task->getOneById('7');
-var_dump($t);
+
+$controller_name = ucfirst(strtolower($_GET['controller'])) . 'Controller';
+$action_name = strtolower($_GET['action']);
+
+
+$controller_instance = new $controller_name();
+
+$controller_instance->$action_name();
